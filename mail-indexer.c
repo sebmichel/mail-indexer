253c3d19097d8426abb91720a3b0820a256f50a1
Sébastien Michel <sebastien.michel@atos.net>
2012-10-10 11:06:04 +0200

    deduce the type of application/octet-stream part from its filename index only message parts for which type is in a list

--- mail-indexer.c
+++ mail-indexer.c
@@ -10,8 +10,6 @@
 #include <json.h>
 
 
-enum part_type { MESSAGE_PART, MESSAGE_PARTIAL, MULTIPART, PART };
-
 struct mail_part {
     struct json_object *mail_tree;     /* global mail tree in JSON */
     int depth;                  /* depth of the current part */
@@ -22,13 +20,92 @@ struct mail_part {
 
 };
 
+struct ct_type {
+    const char *type;
+    const char *subtype;
+};
+struct file_type_mapping {
+    const char **suffix;
+    struct ct_type *ct;
+};
+
+#define S (const char *[])
+#define T (struct ct_type[])
+static struct file_type_mapping mappings[] = {
+    { S{ "txt", NULL }, T{ {"text","plain"}, {"application","txt"}, {NULL,NULL} } },
+    { S{ "html", "htm", NULL }, T{ {"text","html"}, {NULL,NULL} } },
+    { S{ "c", NULL },   T{ {"text","x-csrc"}, {NULL,NULL} } },
+    { S{ "pdf", NULL }, T{ {"application","pdf"}, {"application","x-pdf"}, {"text","pdf"}, {"text","x-pdf"}, {NULL,NULL} } },
+    { S{ "rtf", NULL }, T{ {"application","rtf"}, {"application","x-rtf"}, {"text","rtf"}, {"text","richtext"}, {NULL,NULL} } },
+    { S{ "doc", NULL }, T{ {"application","msword"}, {"application","x-msword"}, {"application","vnd.msword"}, {"application","vnd.ms-word"}, {NULL,NULL} } },
+    { S{ "docx", NULL },T{ {"application","vnd.openxmlformats-officedocument.wordprocessingml.document"}, {NULL,NULL} } },
+    { S{ "xls", NULL }, T{ {"application","vnd.ms-excel"}, {"application","msexcel"}, {"application","x-msexcel"}, {NULL,NULL} } },
+    { S{ "xlsx", NULL },T{ {"application","vnd.openxmlformats-officedocument.spreadsheetml.sheet"}, {NULL,NULL} } },
+    { S{ "ppt", NULL }, T{ {"application","vnd.ms-powerpoint"}, {"application","mspowerpoint"}, {"application","ms-powerpoint"}, {"application","x-mspowerpoint"}, {NULL,NULL} } },
+    { S{ "pptx", NULL },T{ {"application","vnd.openxmlformats-officedocument.presentationml.presentation"}, {NULL,NULL} } },
+    { S{ "odt", NULL }, T{ {"application","vnd.oasis.opendocument.text"}, {NULL,NULL} } },
+    { S{ "ods", NULL }, T{ {"application","vnd.oasis.opendocument.spreadsheet"}, {NULL,NULL} } },
+    { S{ "odp", NULL }, T{ {"application","vnd.oasis.opendocument.presentation"}, {NULL,NULL} } },
+    { NULL, NULL }
+};
+
 extern int optind;
 static int debug = 0;
 
+static void usage(const char *name, int error);
+
+
+static void get_content_type_from_filename(const char *filename,
+                                           GMimeContentType **content_type)
+{
+    int i = -1, j;
+    const char *suffix;
+
+    /* can't deduce the content type */
+    if (filename == NULL) {
+        *content_type = NULL;
+        return;
+    }
+
+    suffix = strrchr(filename, '.');
+
+    if (suffix++) {
+        while (mappings[++i].suffix) {
+            for (j = 0; mappings[i].suffix[j]; j++)
+                if (!strcasecmp(suffix, mappings[i].suffix[j])) {
+                    g_mime_content_type_set_media_type(*content_type, mappings[i].ct[0].type);
+                    g_mime_content_type_set_media_subtype(*content_type, mappings[i].ct[0].subtype);
+                    return;
+                }
+        }
+    }
+}
+
+static int is_indexable(GMimeContentType *content_type, const char *filename)
+{
+    int i = -1, j;
+
+    /* try to find the real content-type from the attachment file name */
+    if (g_mime_content_type_is_type(content_type, "application", "octet-stream"))
+        get_content_type_from_filename(filename, &content_type);
+
+    /* should we use lib magic for unknown content type */
+    if (!content_type)
+        return 0;
+
+    while (mappings[++i].ct) {
+        for (j = 0; mappings[i].ct[j].type; j++)
+            if (g_mime_content_type_is_type(content_type,
+                                            mappings[i].ct[j].type,
+                                            mappings[i].ct[j].subtype))
+                return 1;
+    }
+
+    return 0;
+}
 
 /*
- * format the given part to JSON and add the node to the JSON tree.
- * return the key of the JSON node in the tree
+ * Format the given part to JSON and add the node to the JSON tree.
  */
 static void format_part(GMimeObject *part, struct mail_part *info)
 {
@@ -38,35 +115,35 @@ static void format_part(GMimeObject *part, struct mail_part *info)
     GByteArray *bodypart;
     GMimeDataWrapper *wrapper;
     struct json_object *node;
-    const char *str;
+    const char *filename;
 
-    node = json_object_new_object();
     content_type = g_mime_object_get_content_type(part);
-
-    if (content_type != NULL) {
-        str = g_mime_content_type_to_string(content_type);
-        json_object_object_add(node, "content-type", json_object_new_string(str));
-    }
-    if ((str = g_mime_part_get_filename((GMimePart *)part)) != NULL) {
-        json_object_object_add(node, "filename", json_object_new_string(str));
-    }
+    filename = g_mime_part_get_filename((GMimePart *)part);
 
     /*
-     * prepare the output stream with filters
+     * add headers of the part
+     */
+    node = json_object_new_object();
+    json_object_object_add(node, "content-type",
+                           json_object_new_string(g_mime_content_type_to_string(content_type)));
+    if (filename)
+        json_object_object_add(node, "filename", json_object_new_string(filename));
+
+
+    /*
+     * add body of the part
      */
     outstream = g_mime_stream_mem_new();
     filtered_stream = g_mime_stream_filter_new(outstream);
 
-    /*
-     * filter text in UTF-8 and binary in base64
-     */
+    /* add filters to convert text in UTF-8 and binary in base64 */
     if (g_mime_content_type_is_type(content_type, "text", "*")) {
         GMimeFilter *decode_filter;
         GMimeFilter *charset_filter;
         const char *charset;
 
         /* default charset for text/plain is ASCII
-         * choose arbitrarily ISO-8859-1 for others */
+         * choose arbitrarily ISO-8859-1 for others content type */
         charset = g_mime_content_type_get_parameter(content_type, "charset");
         if (charset == NULL) {
             if (g_mime_content_type_is_type(content_type, "text", "plain"))
@@ -92,18 +169,14 @@ static void format_part(GMimeObject *part, struct mail_part *info)
         g_object_unref(binary_filter);
     }
 
-    /*
-     * apply filters
-     */
+    /* apply filters */
     wrapper = g_mime_part_get_content_object(GMIME_PART(part));
     g_mime_data_wrapper_write_to_stream(wrapper, filtered_stream);
     g_mime_stream_flush(filtered_stream);
     g_object_unref(filtered_stream);
     g_mime_stream_reset(outstream);
 
-    /*
-     * add result to JSON tree
-     */
+    /* add result to JSON tree */
     bodypart = g_mime_stream_mem_get_byte_array((GMimeStreamMem*) outstream);
     json_object_object_add(node, "body",
                            json_object_new_string_len((const char *)bodypart->data, (int)bodypart->len));
@@ -111,7 +184,7 @@ static void format_part(GMimeObject *part, struct mail_part *info)
     g_object_unref(outstream);
 }
 
-static void parse_part(GMimeObject *parent, GMimeObject *part, gpointer rock)
+static void process_part(GMimeObject *parent, GMimeObject *part, gpointer rock)
 {
     struct mail_part *cur = rock;
     GMimeContentType *content_type;
@@ -121,49 +194,54 @@ static void parse_part(GMimeObject *parent, GMimeObject *part, gpointer rock)
     parent_content_type = g_mime_object_get_content_type(parent);
 
     if (debug) {
-        fprintf(stdout, "found... %s> %s ",
+        fprintf(stdout, "found... %*.s(%d)%s> %s ", 8*cur->depth, "", cur->depth,
                 parent_content_type ? g_mime_content_type_to_string(parent_content_type) : "null",
                 g_mime_content_type_to_string(content_type));
     }
 
+    /* message/rfc822 or message/news */
     if (GMIME_IS_MESSAGE_PART(part)) {
-        /* message/rfc822 or message/news */
         GMimeMessage *message;
 
         message = g_mime_message_part_get_message((GMimeMessagePart *) part);
         cur->last_part = part;
-        g_mime_message_foreach(message, parse_part, cur);
+        g_mime_message_foreach(message, process_part, cur);
     }
-    else if (GMIME_IS_MESSAGE_PARTIAL(part)) {
-        /* don't handle such very rare type of part */
-    }
-    else if (GMIME_IS_MULTIPART(part)) {
-        /* multipart/mixed, multipart/alternative,
-         * multipart/related, multipart/signed,
-         * multipart/encrypted, etc... */
-    }
-    else if (GMIME_IS_PART(part)) {
-        /* a normal leaf part */
 
-        if ((GMIME_IS_MULTIPART(parent) || GMIME_IS_MESSAGE_PART(parent))
-                && parent == cur->last_part) { /* enter in a new level */
-            cur->depth++;
-            cur->rank = 0;
+    /* don't handle such very rare type of part */
+    else if (GMIME_IS_MESSAGE_PARTIAL(part)) {
+    }
+
+    /* multipart/mixed, multipart/alternative,
+     * multipart/related, multipart/signed,
+     * multipart/encrypted, etc... */
+    else if (GMIME_IS_MULTIPART(part)) {
+    }
+
+    /* a normal leaf part */
+    else if (GMIME_IS_PART(part)) {
+        /* XXX Fix depth that must decrease on exit of multipart */
+        if (GMIME_IS_MULTIPART(parent) || GMIME_IS_MESSAGE_PART(parent)) {
+            /* enter in a new level */
+            if (parent == cur->last_part) {
+                cur->depth++;
+                cur->rank = 0;
+            }
+            /* prefer the last part in a multipart alternative set */
+            else if (g_mime_content_type_is_type(parent_content_type, "multipart", "alternative")) {
+                if (debug)
+                    fprintf(stdout, "(which replace %s) ", cur->last_node);
+                json_object_object_del(cur->mail_tree, cur->last_node);
+            }
         }
         sprintf(cur->id, "part-%d.%d", cur->depth, ++(cur->rank));
 
         if (debug)
-            fprintf(stdout, " [%s]", cur->id);
+            fprintf(stdout, "[%s]", cur->id);
 
-        /* prefer the last part in a multipart alternative set */
-        if (g_mime_content_type_is_type(parent_content_type, "multipart", "alternative")
-            && parent != cur->last_part) {
-            if (debug)
-                fprintf(stdout, ", discard %s and give preference to this one", cur->last_node);
-            json_object_object_del(cur->mail_tree, cur->last_node);
-        }
-
-        format_part(part, cur);
+        /* don't index unknown part type or content-type not in the mappings list */
+        if (is_indexable(content_type, g_mime_part_get_filename((GMimePart *)part)))
+            format_part(part, cur);
     }
     else {
         /* unknown part type ... */
@@ -177,9 +255,9 @@ static void parse_part(GMimeObject *parent, GMimeObject *part, gpointer rock)
 }
 
 /*
- * Return a JSON document to send to Elasticsearch for indexing
+ * Return a JSON document ready to use with Elasticsearch
  */
-static void parse_mail(int fd, struct json_object **es_json_doc)
+static void process_mail(int fd, struct json_object **es_json_doc)
 {
     GMimeStream *mail_stream;
     GMimeParser *parser;
@@ -224,27 +302,12 @@ static void parse_mail(int fd, struct json_object **es_json_doc)
     }
 
     /* add message parts */
-    g_mime_message_foreach(message, parse_part, &rock);
+    g_mime_message_foreach(message, process_part, &rock);
     g_object_unref(message);
 
     *es_json_doc = rock.mail_tree;
 }
 
-static void usage(const char *name, int error)
-{
-    FILE *out = error ? stderr : stdout;
-
-    fprintf(out, "usage: %s [OPTIONS]... [FILE]\n", name);
-    fprintf(out, "Produce JSON for Elasticsearch from mail from FILE,"
-                 " or standard input, to standard output.\n");
-    fprintf(out, "\n");
-    fprintf(out, "  -d,     enable verbose mode\n");
-    fprintf(out, "  -h,     display this help and exit\n");
-    fprintf(out, "\n");
-    fprintf(out, "With no FILE, or when FILE is -, read standard input.\n");
-
-    error ? exit(EX_USAGE) : exit(0);
-}
 
 int main(int argc, char **argv)
 {
@@ -278,16 +341,29 @@ int main(int argc, char **argv)
 
     g_mime_init(0);
 
-    parse_mail(fd, &es_json_doc);
+    process_mail(fd, &es_json_doc);
     if (es_json_doc == NULL) {
         return EXIT_FAILURE;
     }
 
     g_mime_shutdown();
 
-    if (debug)
-        fprintf(stdout, "--\n");
-    fprintf(stdout, "%s", json_object_to_json_string_ext(es_json_doc, JSON_C_TO_STRING_PRETTY));
-
+    fprintf(stdout, "\n%s\n", json_object_to_json_string_ext(es_json_doc, JSON_C_TO_STRING_PRETTY));
     return EXIT_SUCCESS;
+}
+
+static void usage(const char *name, int error)
+{
+    FILE *out = error ? stderr : stdout;
+
+    fprintf(out, "usage: %s [OPTIONS]... [FILE]\n", name);
+    fprintf(out, "Produce JSON for Elasticsearch from mail from FILE,"
+                 " or standard input, to standard output.\n");
+    fprintf(out, "\n");
+    fprintf(out, "  -d,     enable verbose mode\n");
+    fprintf(out, "  -h,     display this help and exit\n");
+    fprintf(out, "\n");
+    fprintf(out, "With no FILE, or when FILE is -, read standard input.\n");
+
+    error ? exit(EX_USAGE) : exit(0);
 }
